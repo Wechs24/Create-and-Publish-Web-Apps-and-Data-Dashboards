96fecb3665ce668fb2a4ef7c158b3d0cf2648429
NamTrn14 <>
2021-12-25 03:16:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title('Out First Streamlit App')
+st.subheader('Introducing Streamlit in Automate Everything with Python')
+st.write('''This is our first Web App.
+Enjoy it!
+''')
+
